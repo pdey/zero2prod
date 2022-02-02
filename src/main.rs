7dfbb8d44f8f003e19faacda28db8f894e080036
Prasenjit Dey <prasenjit.dey@gmail.com>
2022-02-02 23:29:51 +0530

    Implemented health check api

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,8 @@ use zero2prod::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    run().await?.await   
+    let listener = std::net::TcpListener::bind("127.0.0.1:0").expect("Failed to bind an address");
+    let port = listener.local_addr().unwrap().port();
+    println!("Application running on http://127.0.0.1:{}", port);
+    run(listener).await?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,11 +1,14 @@
+use std::net::TcpListener;
+
 // !tests/health_check.rs
 
 #[actix_web::test]
 async fn health_check_works() {
-    spawn_app().await;
-    
+    let address = spawn_app().await;
+
     let client = reqwest::Client::new();
-    let response = client.get("http://127.0.0.1:8000/health_check")
+    let response = client
+        .get(&format!("{}/health_check", address))
         .send()
         .await
         .expect("Failed to send request.");
@@ -14,8 +17,12 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-async fn spawn_app() {
-    let server = zero2prod::run().await.expect("Failed to bind address.");
+async fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener)
+        .await
+        .expect("Failed to bind address.");
     let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
-
